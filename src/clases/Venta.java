1c8a23d5709ec401c1ff575613c3afe3e14e53b4
Mathigol18 <eduardomunive7@gmail.com>
2026-07-10 12:07:47 -0500

    Implement Venta class with attributes and methods
    Added properties and methods for Venta class.

--- src/clases/Venta.java
+++ src/clases/Venta.java
@@ -1,9 +1,55 @@
 package clases;
 
-/**
- *
- * @author HP
- */
 public class Venta {
-    
+    private int codigoVenta,codigoUsuario,codigoSede,codigoMetodoPago;
+    private String fecha;
+    private double total;
+
+    public int getCodigoVenta() {
+        return codigoVenta;
+    }
+
+    public void setCodigoVenta(int codigoVenta) {
+        this.codigoVenta = codigoVenta;
+    }
+
+    public int getCodigoUsuario() {
+        return codigoUsuario;
+    }
+
+    public void setCodigoUsuario(int codigoUsuario) {
+        this.codigoUsuario = codigoUsuario;
+    }
+
+    public int getCodigoSede() {
+        return codigoSede;
+    }
+
+    public void setCodigoSede(int codigoSese) {
+        this.codigoSede = codigoSese;
+    }
+
+    public int getCodigoMetodoPago() {
+        return codigoMetodoPago;
+    }
+
+    public void setCodigoMetodoPago(int codigoMetodoPago) {
+        this.codigoMetodoPago = codigoMetodoPago;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
 }
