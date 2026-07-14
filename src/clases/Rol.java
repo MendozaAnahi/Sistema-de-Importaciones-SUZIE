ccc15bb4acd8d7571bb292c8a05d4a85861ad01a
hemm0510 <161164628+hemm0510@users.noreply.github.com>
2026-07-14 01:35:49 -0500

    Update Rol.java

--- src/clases/Rol.java
+++ src/clases/Rol.java
@@ -1,9 +1,36 @@
 package clases;
 
-/**
- *
- * @author HP
- */
 public class Rol {
-    
+
+    private int rolID;
+    private String nombreRol;
+
+    public Rol() {
+    }
+
+    public Rol(int rolID, String nombreRol) {
+        this.rolID = rolID;
+        this.nombreRol = nombreRol;
+    }
+
+    public int getRolID() {
+        return rolID;
+    }
+
+    public void setRolID(int rolID) {
+        this.rolID = rolID;
+    }
+
+    public String getNombreRol() {
+        return nombreRol;
+    }
+
+    public void setNombreRol(String nombreRol) {
+        this.nombreRol = nombreRol;
+    }
+
+    @Override
+    public String toString() {
+        return nombreRol;
+    }
 }
