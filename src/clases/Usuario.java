148107f8855de00fd9a706ff8e4007c969f6348c
hemm0510 <161164628+hemm0510@users.noreply.github.com>
2026-07-14 01:36:08 -0500

    Update Usuario.java

--- src/clases/Usuario.java
+++ src/clases/Usuario.java
@@ -1,9 +1,77 @@
 package clases;
 
-/**
- *
- * @author HP
- */
 public class Usuario {
-    
+
+    private int usuarioID;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String contrasena;
+    private Rol rol;
+
+    public Usuario() {
+    }
+
+    public Usuario(int usuarioID, String nombre, String apellido,
+                   String correo, String contrasena, Rol rol) {
+        this.usuarioID = usuarioID;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.contrasena = contrasena;
+        this.rol = rol;
+    }
+
+    public int getUsuarioID() {
+        return usuarioID;
+    }
+
+    public void setUsuarioID(int usuarioID) {
+        this.usuarioID = usuarioID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " " + apellido;
+    }
 }
