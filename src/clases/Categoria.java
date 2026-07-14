e9511df7bd2f1d39db5fc817c7fa343abc459b32
hemm0510 <161164628+hemm0510@users.noreply.github.com>
2026-07-14 01:34:57 -0500

    Update Categoria.java

--- src/clases/Categoria.java
+++ src/clases/Categoria.java
@@ -1,9 +1,46 @@
 package clases;
 
-/**
- *
- * @author HP
- */
 public class Categoria {
-    
+
+    private int categoriaID;
+    private String nombreCategoria;
+    private String descripcion;
+
+    public Categoria() {
+    }
+
+    public Categoria(int categoriaID, String nombreCategoria, String descripcion) {
+        this.categoriaID = categoriaID;
+        this.nombreCategoria = nombreCategoria;
+        this.descripcion = descripcion;
+    }
+
+    public int getCategoriaID() {
+        return categoriaID;
+    }
+
+    public void setCategoriaID(int categoriaID) {
+        this.categoriaID = categoriaID;
+    }
+
+    public String getNombreCategoria() {
+        return nombreCategoria;
+    }
+
+    public void setNombreCategoria(String nombreCategoria) {
+        this.nombreCategoria = nombreCategoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return nombreCategoria;
+    }
 }
