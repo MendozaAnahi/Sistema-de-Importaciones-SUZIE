01c2a88c5e4bd291cba256f98332fd730bbbd3ce
hemm0510 <161164628+hemm0510@users.noreply.github.com>
2026-07-15 02:51:05 -0500

    Update DetalleVenta.java

--- src/clases/DetalleVenta.java
+++ src/clases/DetalleVenta.java
@@ -1,9 +1,62 @@
 package clases;
 
-/**
- *
- * @author HP
- */
 public class DetalleVenta {
-    
+
+    private Venta venta;
+    private Producto producto;
+    private int cantidad;
+    private double precioUnitario;
+    private double subtotal;
+
+    public DetalleVenta() {
+    }
+
+    public DetalleVenta(Venta venta, Producto producto,
+                        int cantidad, double precioUnitario, double subtotal) {
+        this.venta = venta;
+        this.producto = producto;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+        this.subtotal = subtotal;
+    }
+
+    public Venta getVenta() {
+        return venta;
+    }
+
+    public void setVenta(Venta venta) {
+        this.venta = venta;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(double precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
 }
