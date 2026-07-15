42cc47e4cef19905890af324c52ea14dad43c6bc
hemm0510 <161164628+hemm0510@users.noreply.github.com>
2026-07-15 02:50:05 -0500

    Update Venta.java

--- src/clases/Venta.java
+++ src/clases/Venta.java
@@ -1,56 +1,79 @@
 package clases;
 
+import java.time.LocalDateTime;
+
 public class Venta {
-    private int codigoVenta,codigoUsuario,codigoSede,codigoMetodoPago;
-    private String fecha;
+
+    private int ventaID;
+    private LocalDateTime fechaVenta;
     private double total;
-    //Holaaaaaa
+    private Cliente cliente;
+    private Usuario usuario;
+    private MetodoPago metodoPago;
 
-    public int getCodigoVenta() {
-        return codigoVenta;
+    public Venta() {
     }
 
-    public void setCodigoVenta(int codigoVenta) {
-        this.codigoVenta = codigoVenta;
+    public Venta(int ventaID, LocalDateTime fechaVenta, double total,
+                 Cliente cliente, Usuario usuario, MetodoPago metodoPago) {
+        this.ventaID = ventaID;
+        this.fechaVenta = fechaVenta;
+        this.total = total;
+        this.cliente = cliente;
+        this.usuario = usuario;
+        this.metodoPago = metodoPago;
     }
 
-    public int getCodigoUsuario() {
-        return codigoUsuario;
+    public int getVentaID() {
+        return ventaID;
     }
 
-    public void setCodigoUsuario(int codigoUsuario) {
-        this.codigoUsuario = codigoUsuario;
+    public void setVentaID(int ventaID) {
+        this.ventaID = ventaID;
     }
 
-    public int getCodigoSede() {
-        return codigoSede;
+    public LocalDateTime getFechaVenta() {
+        return fechaVenta;
     }
 
-    public void setCodigoSede(int codigoSese) {
-        this.codigoSede = codigoSese;
-    }
-
-    public int getCodigoMetodoPago() {
-        return codigoMetodoPago;
-    }
-
-    public void setCodigoMetodoPago(int codigoMetodoPago) {
-        this.codigoMetodoPago = codigoMetodoPago;
-    }
-
-    public String getFecha() {
-        return fecha;
-    }
-
-    public void setFecha(String fecha) {
-        this.fecha = fecha;
+    public void setFechaVenta(LocalDateTime fechaVenta) {
+        this.fechaVenta = fechaVenta;
     }
 
     public double getTotal() {
         return total;
     }
 
-    public void setTotal(int total) {
+    public void setTotal(double total) {
         this.total = total;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public MetodoPago getMetodoPago() {
+        return metodoPago;
+    }
+
+    public void setMetodoPago(MetodoPago metodoPago) {
+        this.metodoPago = metodoPago;
+    }
+
+    @Override
+    public String toString() {
+        return "Venta N° " + ventaID;
     }
 }
