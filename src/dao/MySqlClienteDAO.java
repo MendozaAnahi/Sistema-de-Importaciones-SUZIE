3941d1d0749934648c89015e5387dd364f24200b
cristhian-awa <cristhianbhariancusicunablas@gmail.com>
2026-07-15 23:58:50 -0500

    Hecho por cristhian

--- src/dao/MySqlClienteDAO.java
+++ src/dao/MySqlClienteDAO.java
@@ -1,1 +1,46 @@
+package dao;
 
+import conexion.Conexion;
+import modelo.Cliente;
+import java.sql.*;
+import java.util.ArrayList;
+
+public class ClienteDAO {
+
+    Connection con;
+    PreparedStatement ps;
+    ResultSet rs;
+
+    // Mostrar clientes
+    public ArrayList<Cliente> listar() {
+
+        ArrayList<Cliente> lista = new ArrayList<>();
+
+        String sql = "SELECT * FROM Cliente";
+
+        try {
+            con = Conexion.getConexion();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+
+            while(rs.next()) {
+
+                Cliente c = new Cliente();
+
+                c.setCliente_ID(rs.getInt("Cliente_ID"));
+                c.setNombre(rs.getString("Nombre"));
+                c.setApellido(rs.getString("Apellido"));
+                c.setTelefono(rs.getString("Telefono"));
+                c.setCorreo(rs.getString("Correo"));
+                c.setDireccion(rs.getString("Direccion"));
+
+                lista.add(c);
+            }
+
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+
+        return lista;
+    }
+}
